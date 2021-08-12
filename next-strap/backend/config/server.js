044df6700ee30af578e47b192d7a55524afcf400
Knyon1 <knyonn1@gmail.com>
2021-08-12 16:05:23 -0300

    only

---
+++ next-strap/backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '03393ef55c3e50cb086a18ab65e24a50'),
+    },
+  },
+});
---
+++ next-strap/backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63ba3824-d81b-4fb3-84b5-bff99bb2c876'
+};
